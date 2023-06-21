62744b40fdbd8a18bb1f5eee24d2449910ff18fb
Robert <roberttv17@gmail.com>
2023-06-21 14:22:23 -0600

    update plantilla

--- src/components/pdf/Plantilla.jsx
+++ src/components/pdf/Plantilla.jsx
@@ -2,13 +2,15 @@ import { Page, Document, StyleSheet } from '@react-pdf/renderer'
 import Encabezado from './Encabezado'
 import PiePagina from './PiePagina'
 
+// import './styles/Plantilla.css'
+
 // Create styles
 const styles = StyleSheet.create({
   page: {
     display: 'flex',
     flexDirection: 'column',
     backgroundColor: '#ffff',
-    /* marginTop: '0.1653543in',
+   /*  marginTop: '0.1653543in',
     marginLeft: '0.984252in',
     marginBottom: '0.787402in',
     marginRight: '0.787402in',  */
